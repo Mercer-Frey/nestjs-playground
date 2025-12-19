98c554c30ccd2412653950a5b5c720b64399a327
Mercer-Frey <40339163+Mercer-Frey@users.noreply.github.com>
2025-12-19 11:25:56 +0200

    logging

--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -7,14 +7,14 @@ export class JobsService {
 
   @Cron(CronExpression.EVERY_5_SECONDS)
   handleCron() {
-    this.logger.log('JobsService running EVERY_5_SECONDS');
+    // this.logger.log('JobsService running EVERY_5_SECONDS');
   }
   @Interval(2000)
   handleInterval() {
-    this.logger.log('JobsService running each 2 sec');
+    // this.logger.log('JobsService running each 2 sec');
   }
   @Timeout(5000)
   handleTimeOut() {
-    this.logger.log('JobsService runs 1 time after 5 sec');
+    // this.logger.log('JobsService runs 1 time after 5 sec');
   }
 }
