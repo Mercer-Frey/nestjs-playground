63aa32d4686290ff4799f63e1bacc7803146b196
Mercer-Frey <40339163+Mercer-Frey@users.noreply.github.com>
2025-12-19 06:24:02 +0200

    CORSes

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,19 @@ import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { SwaggerConfig } from '@root/config/swagger.config';
 import cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get(ConfigService);
   app.use(cookieParser());
 
   app.enableCors({
     credentials: true,
-    origin: 'http://localhost:4200',
+    origin: config.getOrThrow<string>('ALLOWED_ORIGINS').split(','),
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    exposeHeaders: ['Set-Cookie', 'HttpOnly'],
+    allowedHeaders: ['Authorization', 'X-Requested-With'],
   });
   // app.use(LoggerMiddleware);
 
